c2b6838ac7588519993f0512196073ab0c30c0e2
Paul Tanasa <paul@programmer.net>
2020-07-17 01:09:25 +0300

    [ADD] Sample dataset

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ module.exports = new Recaptcha();
 
 const app = require('express')();
 
+app.use('/recaptcha/dataset', module.exports.sample());
+app.use('/recaptcha/client', module.exports.client());
+app.use('/recaptcha', module.exports.server());
+
 app.get('/', (req, res) => {
   res.send('hi');
 });
--- src/recaptcha.js
+++ src/recaptcha.js
@@ -1,20 +1,26 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs-extra');
 const uuid = require('uuid');
 
 class Recaptcha {
   constructor() {
-    this.options = {
-      dataset: [],
-    };
-  
     this.captcha = {};
+
+    const samplePath = path.join(__dirname, '../dataset/sample/sample.json');
+    this.dataset = fs.readJsonSync(samplePath);
   }
 
-  answer(id, answer) {
-
+  check(id) {
+    
   }
 
-  client(request, response, next) {
-
+  client() {
+    const app = express.Router();
+    const clientDir = path.join(__dirname, '../client');
+    
+    app.use(express.static(clientDir));
+    return app;
   }
 
   generateId() {
@@ -31,6 +37,7 @@ class Recaptcha {
       puzzleData: null,
       puzzleExample: null,
       solvedAt: null,
+      verified: false,
     };
 
     return id;
@@ -44,17 +51,47 @@ class Recaptcha {
     }
   }
 
-  setOptions(options) {
-    if (!(options && (typeof options === 'object'))) {
-      throw `Invalid argument 'options' (expected: object | found: ${typeof(options)})`;
-    }
+  sample() {
+    const app = express.Router();
+    
+    app.get('/:image', async (request, response, next) => {
+      const sampleDir = path.join(__dirname, '../dataset/sample');
+      const imagePath = path.join(sampleDir, request.params.image);
 
-    if (Object.prototype.hasOwnProperty.call(options, 'dataset') && (options.dataset instanceof Array)) {
-      this.options.dataset = options.dataset;
-    }
+      if (request.params.image !== 'sample.json' && (await fs.pathExists(imagePath))) {
+        response.contentType('image/png');
+        fs.createReadStream(imagePath).pipe(response);
+      } else {
+        next();
+      }
+    });
+
+    return app;
   }
 
-  verify(request, response, next) {
+  server() {
+    const app = express.Router();
+  
+    app.post('/id', (request, response) => {
+      response.json({
+        data: this.generateId(),
+        statusCode: 201
+      });
+    });
+  
+    app.post('/puzzle', (request, response) => {
+      response.json({
+        data: this.generatePuzzle(request.body.id),
+        statusCode: 201
+      });
+    });
+  
+    app.post('/solve', (request, response) => {
+      
+    });
+  }
+
+  solve(id, answer) {
 
   }
 }
